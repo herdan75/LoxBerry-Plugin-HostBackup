3e70d7b42c6d45b17e60b7c9e6440a8bfafdcd24
herdan <161381188+herdan75@users.noreply.github.com>
2026-05-23 14:05:18 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -13,15 +13,6 @@ chmod 755 "$BACKEND" 2>/dev/null || true
 chmod 755 "$CGI" 2>/dev/null || true
 chmod 755 "$RESTORE" 2>/dev/null || true
 
-SUDOERS_FILE="$LBHOMEDIR/system/sudoers/LoxBerryHostBackup"
-
-echo "# Managed by LoxBerry Host Backup." > "$SUDOERS_FILE"
-echo "loxberry ALL=(root) NOPASSWD: $BACKEND *" >> "$SUDOERS_FILE"
-echo "www-data ALL=(root) NOPASSWD: $BACKEND *" >> "$SUDOERS_FILE"
-
-chmod 644 "$SUDOERS_FILE"
-chown root:root "$SUDOERS_FILE"
-
 sudo -n "$BACKEND" install-schedule 2>/dev/null || true
 
 exit 0
